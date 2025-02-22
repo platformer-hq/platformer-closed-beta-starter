2119dcb153d5f8fd0d487b9b9276c5033ca2b3ef
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-02-22 22:36:32 +0500

    add deploy scripts, fix base path when building

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
+  base: '/platformer-closed-beta-starter',
   plugins: [mkcert()],
 });
